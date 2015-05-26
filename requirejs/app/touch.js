f6839c9097f0300e5b6041d0d7a448020f00bfec
Adrien LOGUT <adrien.logut@ensimag.grenoble-inp.fr>
2015-05-26 16:53:27 +0200

    Correction bis

--- requirejs/app/touch.js
+++ requirejs/app/touch.js
@@ -14,21 +14,29 @@ define(['phaser', 'app/phasergame', 'app/player'], function (Phaser, PhaserGame,
         buttonfire.fixedToCamera = true;
         buttonfire.events.onInputOver.add(function () { Player.fireActive = true; });
         buttonfire.events.onInputOut.add(function () { Player.fireActive = false; });
+        buttonfire.events.onInputDown.add(function () { Player.fireActive = true; });
+        buttonfire.events.onInputUp.add(function () { Player.fireActive = false; });
 
         buttonleft = PhaserGame.game.add.button(0, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
         buttonleft.fixedToCamera = true;
         buttonleft.events.onInputOver.add(function () { Player.moveLeft = true; });
         buttonleft.events.onInputOut.add(function () { Player.moveLeft = false; });
+        buttonleft.events.onInputDown.add(function () { Player.moveLeft = true; });
+        buttonleft.events.onInputUp.add(function () { Player.moveLeft = false; });
 
         buttonright = PhaserGame.game.add.button(160, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
         buttonright.fixedToCamera = true;
         buttonright.events.onInputOver.add(function () { Player.moveRight = true; });
         buttonright.events.onInputOut.add(function () { Player.moveRight = false; });
+        buttonright.events.onInputDown.add(function () { Player.moveRight = true; });
+        buttonright.events.onInputUp.add(function () { Player.moveRight = false; });
 
         buttondown = PhaserGame.game.add.button(96, 536, 'buttonvertical', null, this, 0, 1, 0, 1);
         buttondown.fixedToCamera = true;
         buttondown.events.onInputOver.add(function () { Player.changeColor = true; });
         buttondown.events.onInputOut.add(function () { Player.changeColor = false; });
+        buttondown.events.onInputDown.add(function () { Player.changeColor = true; });
+        buttondown.events.onInputUp.add(function () { Player.changeColor = false; });
 
     }
 
