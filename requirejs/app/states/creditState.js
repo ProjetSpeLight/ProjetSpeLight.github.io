54c3c1f4425dbdf28e0b70f4f17ed64f6cc1a67e
DavidTordjman <david.tordjman@outlook.fr>
2015-06-11 14:57:02 +0200

    Suppression de code copier coller

--- requirejs/app/states/creditState.js
+++ requirejs/app/states/creditState.js
@@ -7,17 +7,12 @@
             // Background with the credits
             var title = this.game.add.sprite(0, 0, 'Credits');
 
-            if (!this.game.device.desktop) {
-                Touch.stop();
-            }
-
             // Button to come back to the Main Menu
             var button_retour = this.add.button(700, 60, 'retour', this.returnMenu, this, 1, 0, 1);
             button_retour.name = 'play';
             button_retour.anchor.setTo(0.5, 0.5);
             button_retour.scale.x = 0.7;
             button_retour.scale.y = 0.7;
-
         },
 
 
