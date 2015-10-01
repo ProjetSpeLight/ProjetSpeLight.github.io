8dc49f517278c2c5af4832e70eb423f9e4f398d6
Adrien LOGUT <adrien.logut@ensimag.grenoble-inp.fr>
2015-10-01 11:39:56 -0400

    Suppression de l'event qui revient au menu

--- requirejs/app/states/preludeState.js
+++ requirejs/app/states/preludeState.js
@@ -66,6 +66,7 @@
         passer: function () {
             if (this.passed) {
                 Intro.finIntro();
+                PhaserGame.game.input.mouse.mouseUpCallback = null;
             } else {
                 this.passed = true;
                 style = { font: "40px Arial", fill: "#ffffff", align: "center" };
